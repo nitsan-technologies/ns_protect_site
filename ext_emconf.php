57973aa2348bb8f1ad28bdc2eecd36f642960670
NITSAN <sanjay@nitsan.in>
2019-12-30 17:10:41 +0530

    [TODO] Configure automatic deployment system

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Protect site',
-    'description' => 'This extension will help you to quickly password protected TYPO3 pages without frontend users management, Check manual for more details. Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/password-protect-site-typo3-plugin/',
+    'title' => '[NITSAN] Password Protect TYPO3 Plugin',
+    'description' => 'This extension will help you to quickly password protected TYPO3 pages without frontend users management, Check manual for more details. Live-Demo: https://demo.t3terminal.com/t3t-extensions/general-utility/protect-site?inavlid=1&type=88889 You can download PRO version for more-features & free-support at https://t3terminal.com/password-protect-site-typo3-plugin/',
     'category' => 'plugin',
     'author' => 'T3: Milan Rathod, QA: Siddharth Sheth',
     'author_company' => 'NITSAN Technologies Pvt Ltd',

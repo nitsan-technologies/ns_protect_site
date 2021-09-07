1a572f5bb26a1f08569ceef9938ea288f0162a22
NITSAN Technologies <sanjay@nitsan.in>
2021-09-07 19:55:45 +0530

    Merged TYPO3 8 Salt method
    [Improve] Code-improve for the password salt method

--- Classes/Controller/ProtectPagesController.php
+++ Classes/Controller/ProtectPagesController.php
@@ -82,6 +82,12 @@ class ProtectPagesController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCon
             }
         } else {
             if (version_compare(TYPO3_branch, '9.4', '<')) {
+                if (\TYPO3\CMS\Saltedpasswords\Utility\SaltedPasswordsUtility::isUsageEnabled('FE')) {
+                    $objSalt = \TYPO3\CMS\Saltedpasswords\Salt\SaltFactory::getSaltingInstance(NULL);
+                    if (is_object($objSalt)) {
+                        $saltedPassword = $objSalt->getHashedPassword($saltedPassword);
+                    }
+                }
                 $objSalt = \TYPO3\CMS\Saltedpasswords\Salt\SaltFactory::getSaltingInstance($saltedPassword, 'BE');
             } else {
                 $objSalt = (new \TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory)->get($saltedPassword, 'BE');

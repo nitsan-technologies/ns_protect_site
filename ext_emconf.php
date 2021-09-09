8ea51aa03d3c5d98924ae92115f80f8b5cb8336c
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-09-09 10:34:33 +0530

    [TODO] Author name change

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Password Protected TYPO3',
     'description' => 'This extension will help you to quickly password protected TYPO3 pages without frontend users management, Check manual for more details. Live-Demo: https://demo.t3terminal.com/t3t-extensions/general-utility/protect-site?inavlid=1&type=88889 You can download PRO version for more-features & free-support at https://t3terminal.com/password-protect-site-typo3-plugin/',
     'category' => 'plugin',
-    'author' => 'T3: Milan Rathod, QA: Siddharth Sheth',
+    'author' => 'NITSAN Technologies Pvt Ltd',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'constraints' => [
         'depends' => [
             'typo3' => '8.0.0-10.9.99',

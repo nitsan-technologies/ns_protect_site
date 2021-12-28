93a2c67bc7aacc88d737eb1fac2a77ecedc89e03
Maulik Lakhnotra <nitsan-gitlab@fischer-and-friends.de>
2021-12-28 10:26:53 +0530

    [TODO] Version compatibility with 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,19 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+if (version_compare(TYPO3_branch, '10.0', '>=')) {
+    $moduleClass = \Nitsan\NsProtectSite\Controller\ProtectPagesController::class;
+} else {
+    $moduleClass = 'ProtectPages';
+}
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Nitsan.NsProtectSite',
     'Nsprotectsite',
     [
-        'ProtectPages' => 'form,login,load'
+        $moduleClass => 'form,login,load'
     ],
     // non-cacheable actions
     [
-        'ProtectPages' => 'login'
+        $moduleClass => 'login'
     ]
 );

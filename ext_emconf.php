50a666478f976bbc18839f48f382f06527cff104
Harsora Krupa <harsorakrupa.nitsan@gmail.com>
2024-05-02 11:17:50 +0530

    Change title and icon

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Password Protected TYPO3',
-    'description' => 'This extension will help you to quickly password protected TYPO3 pages without frontend users management, Check manual for more details. Live-Demo: https://demo.t3terminal.com/t3t-extensions/general-utility/protect-site?inavlid=1&type=88889 You can download PRO version for more-features & free-support at https://t3terminal.com/password-protect-site-typo3-plugin/',
+    'title' => 'Password Protected TYPO3',
+    'description' => 'This extension will help you to quickly password protected TYPO3 pages without frontend users management, Check manual for more details.',
     'category' => 'plugin',
     'author' => 'NITSAN Technologies Pvt Ltd',
     'author_company' => 'NITSAN Technologies Pvt Ltd',

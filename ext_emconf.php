1e090e1bc05b875d7481fe73659f31f87b29c497
NITSAN <sanjay@nitsan.in>
2025-06-25 13:27:20 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Password Protected TYPO3',
-    'description' => 'This extension will help you to quickly password protected TYPO3 pages without frontend users management, Check manual for more details.',
+    'title' => 'TYPO3 Page Password Protection',
+    'description' => 'Easily protect your TYPO3 pages with a password without the need for complex frontend user management. Ideal for temporary content access control during development or limited access pages.',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
-    'author_company' => 'NITSAN Technologies Pvt Ltd',
-    'author_email' => 'sanjay@nitsan.in',
+    'author' => 'Team T3Planet',
+    'author_company' => 'T3Planet',
+    'author_email' => 'info@t3planet.de',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
